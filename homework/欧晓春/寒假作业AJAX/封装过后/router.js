51f00d7cae9a0d20469e45f6b81f1c4f5b504af5
oxc <524112857@qq.com>
2017-01-29 20:26:22 +0800

    寒假作业

---
+++ homework/欧晓春/寒假作业AJAX/封装过后/router.js
@@ -0,0 +1,63 @@
+/**
+ * 使用范例
+ */
+
+
+/**
+ * 发送 GET 请求， 无参数
+ * GET /hello
+ * 返回响应数据
+ */
+app.get('/more', function(req, res) {
+	var a = [];
+	var b = req.query
+    for(var i = 0; i < 3; i++){
+    	a.push(parseInt(b.len) + i + 1)
+    }
+	res.send(a.join(','));
+});
+
+
+
+
+/**
+ * 发送 GET 请求, 有参数
+ * GET /user/100
+ * query = { name: 'ruoyu', age: 28 }
+ */
+app.get('/user/:uid', function(req, res) {
+	console.log(req.params.uid); //100
+	console.log(req.query.name); // 'ruoyu'
+	res.send({
+		status: 1,
+		errorMsg: "请先注册"
+	});
+});
+
+
+/**
+ * 发送 POST 请求， 有参数
+ * POST /comment
+ * query = { comment: "这是评论内容" }
+ */
+app.post('/more', function(req, res) {
+	var a = [];
+	var b = req.body
+    for(var i = 0; i < 3; i++){
+    	a.push(parseInt(b.len) + i + 1)
+    }
+	res.send(a.join(','));
+});
+
+
+
+/**
+ * 页面路由，从模板渲染页面渲染页面, 
+ * htttp://localhost:8080/user
+ * 支持 ejs, jade 模板
+ */
+app.get('/user', function(req, res) {
+	res.render('user.ejs', {
+		username: '饥人谷'
+	});
+});
