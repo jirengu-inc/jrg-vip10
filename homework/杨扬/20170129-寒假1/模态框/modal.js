8d1f1239a93e2ccf4150fe49ecc928f893ff715b
NathanYangcn <nathanyang1991@163.com>
2017-01-29 23:31:34 +0800

    push homework ：2017/01/寒假作业1

--- homework/杨扬/20170129-寒假1/模态框/modal.js
+++ homework/杨扬/20170129-寒假1/模态框/modal.js
@@ -13,23 +13,21 @@ function openHandler() {
 function closeHandler() {
     modal.classList.remove('show');
 }
-
-btn.addEventListener('click', function(e) { // 点击按钮打开modal
+// 点击按钮打开modal
+btn.addEventListener('click', function(e) { 
     e.stopPropagation();
     openHandler();
 });
-
-for(var i = 0; i < close.length; i++){ // 点击按钮关闭modal
+// 点击按钮关闭modal
+for(var i = 0; i < close.length; i++){ 
     close[i].addEventListener('click', function (e) {
         e.stopPropagation();
         closeHandler();
     });
 }
-
-document.body.addEventListener('click', closeHandler); // 点击以为modal关闭
-modalCt.addEventListener('click', function (e) { // 点击modal不关闭
+// 点击modal以外modal关闭；点击modal不关闭
+document.body.addEventListener('click', closeHandler); 
+modalCt.addEventListener('click', function (e) {
     e.stopPropagation();
     openHandler();
 });
-
-// 注释：事件特性：事件传递到目标元素后就开始回城，例如：监听body，到了body，就开始返回，此时只要拦截body内的相同事件不到达body即可不处罚body程序
